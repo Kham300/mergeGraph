95b7a987ceb650c75c2bdf48cac0ef10b8d5c65e
ivanlardis <ivan24031993@yandex.ru>
2016-03-11 11:48:29 +0300

    clickok

--- app/src/main/java/com/lardis/ivan/testcustomview/MainActivity.java
+++ app/src/main/java/com/lardis/ivan/testcustomview/MainActivity.java
@@ -2,12 +2,30 @@ package com.lardis.ivan.testcustomview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    MyView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        view = (MyView) findViewById(R.id.myview);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().isEmpty()) {
+                    view.setN(Integer.parseInt(editText.getText().toString()));
+                    view.invalidate();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/lardis/ivan/testcustomview/MyView.java
+++ app/src/main/java/com/lardis/ivan/testcustomview/MyView.java
@@ -1,189 +1,192 @@
 package com.lardis.ivan.testcustomview;
 
 import android.content.Context;
-import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * TODO: document your custom view class.
  */
 public class MyView extends View {
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-    private Drawable mExampleDrawable;
+    Paint mPaint = new Paint();
+    float minX = 0;
+    float maxX;
+    int npointTouch = -1;
+    float minWidthBlock;
+    float minWidthBorder;
+    float nHideMonitor = 5;
+    boolean isScroll;
+    float leftBlok;
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    public void setN(int n) {
+        this.n = n;
+        this.x = 0;
+    }
+
+
+    int n = 5;
 
     public MyView(Context context) {
         super(context);
-        init(null, 0);
+        init();
     }
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init();
     }
 
-    public MyView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.MyView, defStyle, 0);
+    //    public MyView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+    public void init() {
 
-        mExampleString = a.getString(
-                R.styleable.MyView_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.MyView_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.MyView_exampleDimension,
-                mExampleDimension);
 
-        if (a.hasValue(R.styleable.MyView_exampleDrawable)) {
-            mExampleDrawable = a.getDrawable(
-                    R.styleable.MyView_exampleDrawable);
-            mExampleDrawable.setCallback(this);
-        }
-
-        a.recycle();
-
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
-    }
-
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
 
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
+        minWidthBlock = getWidth() / nHideMonitor;
+        minWidthBorder = minWidthBlock / 3;
+        mPaint.setStrokeWidth(10);
+        maxX = (n - nHideMonitor) * minWidthBlock + minWidthBorder * 2;
+        isScroll = !(minWidthBlock < getWidth() / n);
 
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
 
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
+        if (isScroll) {
+            leftBlok = minWidthBlock;
+        } else {
+
+            leftBlok = (getWidth() - minWidthBorder * 2) / n;
+
         }
+        for (int i = 0; i < n; i++) {
+
+
+            {
+                if (i % 2 == 0) mPaint.setColor(Color.BLUE);
+                if (i % 2 == 1) mPaint.setColor(Color.GREEN);
+                canvas.drawRect(minWidthBorder + x + leftBlok * i, 0, minWidthBorder + x + leftBlok * (i + 1), getHeight(), mPaint);
+
+            }
+
+
+        }
+
+
+        //border
+        mPaint.setColor(Color.GRAY);
+        canvas.drawRect(getWidth() - minWidthBorder, 0, getWidth(), getHeight(), mPaint);
+        canvas.drawRect(0, 0, minWidthBorder, getHeight(), mPaint);
+        canvas.drawRect(0, getHeight() - minWidthBorder, getWidth(), getHeight(), mPaint);
+//border
+
+// дорисовка выделенной
+        mPaint.setColor(Color.YELLOW);
+        float leftrect = minWidthBorder + x + leftBlok * npointTouch;
+        float rightrect = minWidthBorder + x + leftBlok * (npointTouch + 1);
+
+        if (!(rightrect < minWidthBorder && leftrect > getWidth() - minWidthBorder)) {
+
+            if (leftrect < minWidthBorder) leftrect = minWidthBorder;
+            if (rightrect > getWidth() - minWidthBorder) rightrect = getWidth() - minWidthBorder;
+            canvas.drawRect(leftrect, 0, rightrect, getHeight() - minWidthBorder, mPaint);
+        }
+        Log.d("Mylog","rightrect="+rightrect+"leftrect="+leftrect);
+        if((rightrect-leftrect)>15)
+        {
+            Path mPath = new Path();
+            mPaint.setColor(Color.BLUE);
+
+            mPath.moveTo(leftrect, getHeight());
+            mPath.lineTo(rightrect, getHeight());
+            mPath.lineTo((leftrect + rightrect) / 2, getHeight() - minWidthBorder);
+//canvas.
+            canvas.drawPath(mPath, mPaint);
+        }
+//
+
+
+        //стрелки для скрола
+        if (isScroll) {
+            Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.strelka);
+            mBitmap = Bitmap.createScaledBitmap(mBitmap, (int) (minWidthBlock / 3), (int) (minWidthBlock / 3), false);
+
+            canvas.drawBitmap(mBitmap, 0, getHeight() - minWidthBlock, mPaint);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(180);
+            mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+            canvas.drawBitmap(mBitmap, getWidth() - minWidthBlock / 3, getHeight() - minWidthBlock, mPaint);
+
+        }
+
+        //стрелки для скрола
+
     }
 
-    /**
-     * Gets the example string attribute value.
-     *
-     * @return The example string attribute value.
-     */
-    public String getExampleString() {
-        return mExampleString;
+
+    float x;
+    float bufX;
+    float bufmy = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        // TODO Auto-generated method stub
+
+
+        switch (motionEvent.getAction()) {
+            case (MotionEvent.ACTION_DOWN):
+                bufX = x;
+
+                bufmy = motionEvent.getX();
+                Log.d("Mylog", "bufmy" + bufmy);
+                break;
+            case (MotionEvent.ACTION_MOVE):
+                x = bufX - (bufmy - motionEvent.getX());
+
+
+                Log.d("Mylog", "bufmy" + bufmy);
+                break;
+            case (MotionEvent.ACTION_UP):
+                if (Math.abs(bufX - x) < 4) {
+                    if (isScroll)
+                        npointTouch = (int) ((motionEvent.getX() - bufX - minWidthBorder) / minWidthBlock);
+                    else
+                        npointTouch = (int) ((motionEvent.getX() - minWidthBorder - bufX) * n / (getWidth() - minWidthBorder * 2));
+
+//                    (minWidthBorder + x + (getWidth() - minWidthBorder * 2) * i / n, 0, minWidthBorder + x + (getWidth() - minWidthBorder * 2) * (i + 1) / n, getHeight(), mPaint);
+
+                }
+                x = bufX - (bufmy - motionEvent.getX());
+
+                break;
+
+        }
+        if (x < -maxX) x = -maxX;
+        if (x > minX) x = minX;
+        invalidate();
+
+
+        return true;
     }
 
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
 
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
-    }
-
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
-    }
 }
