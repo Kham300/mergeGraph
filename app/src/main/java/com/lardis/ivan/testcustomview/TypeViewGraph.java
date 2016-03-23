4d7efd1011f74ce0f369b2a176a69e2e35884954
ivan <ivan24031993@yandex.ru>
2016-03-23 18:00:18 +0300

    proverkakaka

---
+++ app/src/main/java/com/lardis/ivan/testcustomview/TypeViewGraph.java
@@ -0,0 +1,9 @@
+package com.lardis.ivan.testcustomview;
+
+/**
+ * Created by i.larin on 22.03.2016.
+ */
+public enum TypeViewGraph {
+      MESH_WEEK_ITEM_DAY, MESH_WEEK_ITEM_WEEK, MESH_DAY_ITEM_DAY,MESH_MONTH_ITEM_MONTH,
+    MESH_MONTH_ITEM_WEEK;
+}
